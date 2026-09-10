bf9758951839c84fa5be1af12f50ca1c5444a3af
tanmay5268 <tanmay.mewati94@gmail.com>
2026-09-10 18:36:48 +0530

    Starting over

--- packages/database/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-export * from "./db/schema";
-export { eq } from "drizzle-orm";
-config({ path: ".env" });
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
